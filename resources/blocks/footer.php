f89f9aa25e16565fa4672b647fa63a067a3db7fd
Alex <ibymif2000@yandex.ru>
2017-06-15 22:39:49 +0300

    header

--- resources/blocks/footer.php
+++ resources/blocks/footer.php
@@ -1,3 +1,21 @@
 <footer>
-	
+	<div class="autor">
+		<div>Автор сайта: Lokehoke
+			<a href="https://vk.com/id133127261">
+				<img src="resources/img/vk.svg" class="vk-img">
+			</a>
+		</div>
+		<div>Автор идеи: Futyn
+			<a href="https://vk.com/nikitanosachev">
+				<img src="resources/img/vk.svg" class="vk-img">
+			</a>
+		</div>
+	</div>
+	<div class="about">
+		<a href="/">О проеке</a>
+	</div>
+	<div class="feedback">
+		<a href="/">Обратная связь</a>
+	</div>
+
 </footer>
--- resources/blocks/header.php
+++ resources/blocks/header.php
@@ -1,3 +1,13 @@
 <header>
-	
+	<div class="name-logo">
+		<a href="/"><img src="#"> Wynex</a>
+	</div>
+	<div class="search">
+		<input type="text" name="search">
+	</div>
+	<div class="registr-auth">
+		<a href="/registration">Регистрация</a> | 
+		<a href="/login">Вход</a>
+		</div>
+	<div class="profile"></div>
 </header>
